a849438d4e2ac471fd446915c71b6a0ef6cc625b
Jess Primero <jprimero@packetbender.net>
2022-06-01 12:54:56 +0800

    format noxfile.py according to flake8

--- noxfile.py
+++ noxfile.py
@@ -1,12 +1,15 @@
 import nox
 
+
 @nox.session(python=["3.9"])
 def tests(session):
     args = session.posargs or ["--cov", "-m", "not e2e"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
 
+
 locations = "src", "tests", "noxfile.py"
+
 
 @nox.session(python=["3.9"])
 def lint(session):
