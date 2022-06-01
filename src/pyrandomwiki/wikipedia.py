114258c315dd86771d6c8da88bd6a23115a065ff
Jess Primero <jprimero@packetbender.net>
2022-06-01 12:46:58 +0800

    format wikipedia.py according to flake8

--- src/pyrandomwiki/wikipedia.py
+++ src/pyrandomwiki/wikipedia.py
@@ -3,8 +3,9 @@ import requests
 
 API_URL = "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
 
+
 def random_page(language="en"):
-    url  = API_URL.format(language=language)
+    url = API_URL.format(language=language)
 
     try:
         with requests.get(url) as response:
