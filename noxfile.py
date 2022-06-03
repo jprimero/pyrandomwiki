20addcd2288745b36c8c663f203ef70e083446f0
Jess Primero <jprimero@packetbender.net>
2022-06-03 10:13:57 +0800

    add nox session for the tool safety

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,5 @@
+import tempfile
+
 import nox
 
 
@@ -31,4 +33,20 @@ def black(session):
     session.run("black", *args)
 
 
-nox.options.sessions = "lint", "tests"
+@nox.session(python=["3.9"])
+def safety(session):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install("safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
+
+
+nox.options.sessions = "lint", "safety", "tests"
