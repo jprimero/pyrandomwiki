407888a57218694bb2fecdd5499e893be9ff1634
Jess Primero <jprimero@packetbender.net>
2022-06-07 11:59:41 +0800

    random_page should raise an error when requests.get is None

--- src/tests/test_wikipedia.py
+++ src/tests/test_wikipedia.py
@@ -1,3 +1,8 @@
+from unittest.mock import Mock
+
+import click
+import pytest
+
 from pyrandomwiki import wikipedia
 
 
@@ -10,3 +15,9 @@ def test_random_page_uses_given_language(mock_requests_get):
 def test_random_page_returns_page(mock_requests_get):
     page = wikipedia.random_page()
     assert isinstance(page, wikipedia.Page)
+
+
+def test_random_page_handles_validation_errors(mock_requests_get: Mock) -> None:
+    mock_requests_get.return_value.__enter__.return_value.json.return_value = None
+    with pytest.raises(click.ClickException):
+        wikipedia.random_page()
